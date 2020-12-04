a813b7d0be2a3ee099d056030d833eb78a42fa5a
chhk <chorhatarahuduketuri@gmail.com>
2020-12-04 12:50:09 +0000

    Day 4: second star

--- day4/second_star.py
+++ day4/second_star.py
@@ -1,22 +1,21 @@
 import re
 
-
-def check_height(x):
-    pass
-
+re_hcl = re.compile(r'^#[0-9a-f]{6}$')
+re_ecl = re.compile(r'^(amb|blu|brn|gry|grn|hzl|oth)$')
+re_pid = re.compile(r'^[0-9]{9}$')
+re_hgt_cm = re.compile(r'^\d{3}cm$')
+re_hgt_in = re.compile(r'^\d{2}in$')
 
 valid_fields = {
-    'byr': lambda x: 1920 <= x <= 2002,  # Birth Year'
-    'iyr': lambda x: 2010 <= x <= 2020,  # Issue Year'
-    'eyr': lambda x: 2020 <= x <= 2030,  # Expiration Year'
-    'hgt': lambda x: check_height(x),  # Height'
-    'hcl': lambda x: re.compile(r'^#[0-9a-f]{6}$').match(x),  # Hair Color'
-    'ecl': lambda x: re.compile(r'^(amb|blu|brn|gry|grn|hzl|oth)$').match(x),  # Eye Color'
-    'pid': lambda x: re.compile(r'^[0-9]{9}$').match(x),  # Passport ID'
-    'cid': lambda x: True,  # Country ID'
+    'byr': lambda x: 1920 <= int(x) <= 2002,  # Birth Year
+    'iyr': lambda x: 2010 <= int(x) <= 2020,  # Issue Year
+    'eyr': lambda x: 2020 <= int(x) <= 2030,  # Expiration Year
+    'hgt': lambda x: check_height(x),  # Height
+    'hcl': lambda x: re_hcl.match(x),  # Hair Color
+    'ecl': lambda x: re_ecl.match(x),  # Eye Color
+    'pid': lambda x: re_pid.match(x),  # Passport ID
 }
 
-required = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
 data = []
 
 with open('input.txt', 'r') as f:
@@ -26,9 +25,20 @@ with open('input.txt', 'r') as f:
     for line in lines:
         data.append({k: v for (k, v) in [x.split(':') for x in line.split(' ')]})
 
+
+def check_height(x):
+    if re_hgt_cm.match(x):
+        if 150 <= int(x[0:3]) <= 193:
+            return True
+    if re_hgt_in.match(x):
+        if 59 <= int(x[0:2]) <= 76:
+            return True
+    return False
+
+
 number_of_valid_passports = 0
 for datum in data:
-    if all(field in datum for field in required):
+    if all([(datum.get(k) and v(datum[k])) for (k, v) in valid_fields.items()]):
         number_of_valid_passports += 1
 
-print(f'len(number_of_valid_passports): {number_of_valid_passports}')
+print(number_of_valid_passports)
